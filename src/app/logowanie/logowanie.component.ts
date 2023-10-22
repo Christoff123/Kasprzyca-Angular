e44b1cd64e9619d9518b28ca4c9b29ca626857d4
Christoff123 <krzysztof.kasprzyca@wp.pl>
2023-10-22 17:59:29 +0200

    Jasmine-Test

--- src/app/logowanie/logowanie.component.ts
+++ src/app/logowanie/logowanie.component.ts
@@ -3,16 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-logowanie',
   templateUrl: './logowanie.component.html',
-  styleUrls: ['./logowanie.component.scss']
+  styleUrls: ['./logowanie.component.css']
 })
 export class LogowanieComponent implements OnInit {
+  public title: string;
 
   formData = {
     username: '',
     password: ''
   }
 
-  constructor() { }
+  constructor() { 
+    this.title = 'Witaj na stronie logowania'
+  }
 
   ngOnInit(): void {
   }
